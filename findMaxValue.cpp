3fe5d1b9489d975764756439c9ca7b1244136a92
fazlerabbi13 <mohammad0132794@gmail.com>
2025-01-15 21:56:11 +0600

    find the max value

---
+++ findMaxValue.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << fmax(2.0, 0.25) << "\n";
+  cout << fmax(31.2f, 18.0f) << "\n";
+  cout << fmax(14, 22) << "\n";
+  cout << fmax(96, 2048) << "\n";
+  return 0;
+}
