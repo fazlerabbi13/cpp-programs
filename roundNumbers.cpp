03113cb108eeac5a808511fcab5307d721ef3da2
fazlerabbi13 <mohammad0132794@gmail.com>
2025-01-16 20:24:51 +0600

    round numbers

---
+++ roundNumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << ceil(0.60) << "\n";
+  cout << ceil(0.40) << "\n";
+  cout << ceil(5) << "\n";
+  cout << ceil(5.1) << "\n";
+  cout << ceil(-5.1) << "\n";
+  cout << ceil(-5.9) << "\n";
+  return 0;
+}
