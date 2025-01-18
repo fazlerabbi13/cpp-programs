5a5e0332d8e9b5d302ce7c9a50d83bf69089bc4d
fazlerabbi13 <mohammad0132794@gmail.com>
2025-01-18 21:17:06 +0600

    square root

---
+++ squareRoot.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << sqrt(0) << "\n";
+  cout << sqrt(1) << "\n";
+  cout << sqrt(9) << "\n";
+  cout << sqrt(0.64) << "\n";
+  cout << sqrt(-25) << "\n";
+  return 0;
+}
