0a5d12f30ed792a7878d52b57be6f9dd3037b7fe
fazlerabbi13 <mohammad0132794@gmail.com>
2025-01-30 23:02:46 +0600

    pass string

---
+++ passString.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void modifyStr(string &str) {
+  str += " World!";
+}
+
+int main() {
+  string greeting = "Hello";
+  modifyStr(greeting);
+  cout << greeting;
+  return 0;
+}
