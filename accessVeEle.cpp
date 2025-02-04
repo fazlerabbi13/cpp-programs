e5c7210d77de9b8173fcecc65700addc094bbe55
fazlerabbi13 <mohammad0132794@gmail.com>
2025-02-04 21:45:16 +0600

    access a vector element by referring to the index number inside square brackets [].

---
+++ accessVeEle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  // Create a vector called cars that will store strings
+  vector<string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+  // Get the first element
+  cout << cars[0] << "\n";
+  
+  // Get the second element
+  cout << cars[1] << "\n";
+  return 0;
+}
