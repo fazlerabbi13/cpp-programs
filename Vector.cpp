1bd9fda8261c9a827e1a656d8e06e4c7db5dd6ee
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-27 21:55:24 +0600

    To add an element to the vector, you can use the .push_back() function, which will add an element at the end of the vector

--- Vector.cpp
+++ Vector.cpp
@@ -5,11 +5,22 @@ using namespace std;
 int main(){
     vector<int> numbers = {1,2,3,4,5,6,7,8,9};
 
-    numbers[2]=5;
+    /*
+    To add an element to the vector, you can use the .push_back() function,
+     which will add an element at the end of the vector
+    */
+   numbers.push_back(10);
+    numbers[2]=5;  //change the value of index 2
 
+        // loop throug the vector
     for(int i=0;i<=numbers.size();i++){
         cout<<numbers[i]<<"\n";
     }
+    
+
+    // output the first and last element of vector
+
+    cout<<"print the first and last element of vector"<<"\n";
 
     cout<<numbers.front()<<"\n";
     cout<<numbers.back()<<"\n";
