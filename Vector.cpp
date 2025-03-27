ea57db6216c73239bc4e778a9cdbef75e716b9b6
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-27 21:37:05 +0600

    One advantage of using the vector library, is that it includes many useful functions. For example, you can access the first or the last element of a vector with the .front() and .back() functions

---
+++ Vector.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> numbers = {1,2,3,4,5,6,7,8,9};
+
+    cout<<numbers.front()<<"\n";
+    cout<<numbers.back()<<"\n";
+
+    return 0;
+}
