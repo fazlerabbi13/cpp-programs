698591f35a464762e71c37684278a7e00aa29d7a
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-28 23:26:40 +0600

    in this problem given specific time and ensure maximum visit within this time

---
+++ visitRelatives.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> times(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> times[i];
+    }
+    sort(times.begin(), times.end());
+    
+    int total = 0, count = 0;
+    for (int time : times) {
+        if (total + time <= x) {
+            total += time;
+            ++count;
+        } else {
+            break;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
