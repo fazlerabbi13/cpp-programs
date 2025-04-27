32ea09a14f4b5dd8b9e3cb59ad58cafcf8e80b91
fazlerabbi13 <mohammad0132794@gmail.com>
2025-04-27 21:25:16 +0600

    A set stores unique elements where they:Are sorted automatically in ascending order.Are unique, meaning equal or duplicate values are ignored.

---
+++ setInCpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+  // Create a set called cars that will store strings
+  set<string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+  // Print set elements
+  for (string car : cars) {
+    cout << car << "\n";
+  }
+  return 0;
+}
