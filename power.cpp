87dd0ccd682b339b4d33af4624b4efe6c848d039
fazlerabbi13 <mohammad0132794@gmail.com>
2025-05-03 23:03:15 +0600

    The pow() function raises a number to the power of another number.

---
+++ power.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << pow(2.0f, 8.0f) << "\n";
+  cout << pow(3.0f, 4.0f) << "\n";
+  cout << pow(9.0, 0.5) << "\n";
+  cout << pow(8.0, -1.0) << "\n";
+  cout << pow(10.0f, -2.0f) << "\n";
+  return 0;
+}
---
+++ trailingZero.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    long long zeros = 0;
+    for (long long i = 5; n / i >= 1; i *= 5) {
+        zeros += n / i;
+    }
+
+    cout << zeros << endl;
+    return 0;
+}
+
