30c97e4d7c7a4ff523634ae27a9beeffab2db9f5
fazlerabbi13 <mohammad0132794@gmail.com>
2025-05-15 20:19:46 +0600

    printing the character appearing with maximum frequency. If there are more than 1 such character, print the one with lower ASCII value

---
+++ stringFrequency.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    vector<int> freq(128, 0);  // to store frequency of all ASCII characters
+
+    // Count frequency of each character
+    for (char c : s) {
+        freq[(int)c]++;
+    }
+
+    // Find the character with the highest frequency and lowest ASCII if tied
+    char result = 0;
+    int maxFreq = 0;
+
+    for (int i = 0; i < 128; ++i) {
+        if (freq[i] > maxFreq || (freq[i] == maxFreq && i < result)) {
+            maxFreq = freq[i];
+            result = (char)i;
+        }
+    }
+
+    cout << result << endl;
+    return 0;
+}
