88826064d2cb177bbc3a613c28c03165e2293c92
fazlerabbi13 <mohammad0132794@gmail.com>
2025-06-13 11:28:57 +0600

    Read an integer N. Print the square of each one of the even values from 1 to N including N if it is the case.

---
+++ evenSquare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 2; i <= N; i += 2) {
+        cout << i << "^2 = " << (i * i) << endl;
+    }
+
+    return 0;
+}
