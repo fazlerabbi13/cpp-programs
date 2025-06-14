04c9627d72c2a507e8815005153dc69fef7008df
fazlerabbi13 <mohammad0132794@gmail.com>
2025-06-14 22:39:45 +0600

    Read an undetermined number of pairs of integer values. Write a message for each pair indicating if this two numbers are in ascending or descending order.

---
+++ aeDes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+
+    while (true) {
+        cin >> x >> y;
+
+        if (x == y)
+            break;
+
+        if (x < y)
+            cout << "Crescente" << endl;
+        else
+            cout << "Decrescente" << endl;
+    }
+
+    return 0;
+}
