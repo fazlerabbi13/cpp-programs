f19c8a9c2c31c9149c3a894404c26b80205600c3
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-17 20:35:59 +0600

    finding size of a vector

--- STL/vectorInCpp.cpp
+++ STL/vectorInCpp.cpp
@@ -5,10 +5,17 @@ using namespace std;
 int main(){
     vector<int> vec={1,2,3,4,5};
     vec.push_back(6);
+
+    vector<int> vec1;
+    vec1.push_back(2);
+    
+    cout<<vec1.size()<<"\n";
+    
     for(int i=0;i<vec.size();i++){
         cout<<vec[i]<<" ";
     }
 
+    
 
     return 0;
 }
