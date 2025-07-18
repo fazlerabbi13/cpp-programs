bd9503518514cf85f852965a605e4e9fefac2191
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-18 21:49:46 +0600

    Elements in a deque can be updated by assigning the new value using assignment operator while accessing the element by index.

---
+++ STL/updateInDeque.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    deque<int> dq = {1, 4, 2, 3, 5};
+
+    // Updating element
+    dq[2] = 8;
+    
+    cout << dq[2]; 
+    return 0;
+}
