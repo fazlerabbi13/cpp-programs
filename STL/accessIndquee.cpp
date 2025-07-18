99aa97416f2cc01e18464ab73d52b47704981fcd
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-18 21:48:18 +0600

    Deque provides the front()and back() methods to easily access the first and last element.

---
+++ STL/accessIndquee.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    deque<int> dq = {1, 4, 2, 3, 5};
+
+    // Accessing elements
+    cout << dq[2] << endl;
+    cout << dq.front() << endl;
+    cout << dq.back();
+
+    return 0;
+}
