3e9469f6523e1221e1557dfc2ddaeb26bd724b76
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-19 15:52:04 +0600

    sort vector using 'insertion sort' algorithm

--- STL/incertionSort.cpp
+++ STL/incertionSort.cpp
@@ -11,5 +11,24 @@ int main(){
         cin>>vector[i];
     }
 
+    for(int i=1;i<vector.size();i++){
+        int current=vector[i];
+        int previous=i-1;
+
+        while (previous>=0 && vector[previous]>current)
+        {
+           vector[previous+1]=vector[previous];
+           previous--;
+        }
+        vector[previous+1]=current;
+        
+    }
+
+    for (int i=0; i<vector.size();i++)
+    {
+        cout<<vector[i]<<" ";
+    }
+    
+
     return 0;
 }
